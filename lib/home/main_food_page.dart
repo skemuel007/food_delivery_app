153a96a4d14f636f8a9aed3db2e3ddb65dda9c81
Salvation Kemuel <skemuel007@gmail.com>
2022-02-28 16:18:40 +0100

    Big Text widget created

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/utils/colors.dart';
+import 'package:food_delivery_app/widgets/big_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  _MainFoodPageState createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom:15),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: const [
+                       BigText(
+                         text: 'Nigeria',
+                       ),
+                      Text('City')
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
